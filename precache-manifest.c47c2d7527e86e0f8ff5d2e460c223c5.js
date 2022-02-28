dbf3397ab3803a219ef7f070da1c32a81e6bde0f
Wagner Trezub <60133113+Wagner3UB@users.noreply.github.com>
2022-02-28 17:55:14 +0100

    Add files via upload

---
+++ precache-manifest.c47c2d7527e86e0f8ff5d2e460c223c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60c9cefaf92926ed26ee",
+    "url": "/static/css/main.53cd7470.chunk.css"
+  },
+  {
+    "revision": "60c9cefaf92926ed26ee",
+    "url": "/static/js/main.12b47aa7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5e003977f82ffd97a931",
+    "url": "/static/js/2.52837c4e.chunk.js"
+  },
+  {
+    "revision": "37760980bc258b83ca2adeb9058872e0",
+    "url": "/index.html"
+  }
+];
